912b6a0d55033a850ea9028bc5447f5dcb2b717c
Cameron Smith <cabotmoose@gmail.com>
2014-03-14 18:05:38 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SocialDiscount::Application.config.secret_key_base = '10fc9bf18b1387611175e9e765a2b09c9de17c9adaafd57a25dbe05a4897e2c404b45ac8b3a0ec2a1c37b3eabb2cbd1dc6522a16f3b7aee0b783d5674b8e7370'
